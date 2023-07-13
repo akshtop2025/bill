b4520f6e7b3911e063fd08134d405bd8d3c1d39e
unknown <akshpateltop2025@gmail.com>
2023-07-13 18:09:44 +0530

    bill

--- asset/js.js
+++ asset/js.js
@@ -1,17 +1,66 @@
-function Click() {
+function aksh() {
+    console.log("call")
 
     let rotaryint = document.getElementById("rotary").value
+    let rotaryintt = document.getElementById("rotaryy").value
     let thresher = document.getElementById("thresher").value
+    let thresherr = document.getElementById("thresherr").value
     let kaltiint = document.getElementById("kalti").value
+    let kaltiintt = document.getElementById("kaltii").value
 
+    // let rotaryint = 60
     let rotarytotel = rotaryint * 1300
+    let rotarytotell = rotaryintt * 21.6666
     let threshertotel = thresher * 1200
+    let threshertotell = thresherr * 20
     let kaltitotel = kaltiint * 900
+    let kaltitotell = kaltiintt * 15
 
-    let add = rotarytotel + threshertotel + kaltitotel
+
+
+    let add = parseFloat(rotarytotel) + parseFloat(rotarytotell) + parseFloat(threshertotel) + parseFloat(threshertotell) + parseFloat(kaltitotel) + parseFloat(kaltitotell)
     console.log("add")
 
     let input = document.getElementById("totelbill").value = add
     alert("thanks for use")
 
 }
+
+
+// function Click() {
+
+//     let rotaryint = parseFloat(document.getElementById("rotary").value)
+//     let thresher = document.getElementById("thresher").value
+//     let kaltiint = document.getElementById("kalti").value
+
+//     // let rotaryint = 60
+//     let rotarytotel = rotaryint / 60 * 60
+//     let threshertotel = thresher * 1200
+//     let kaltitotel = kaltiint * 900
+
+
+
+//     let add = parseFloat(rotarytotel) + parseFloat(threshertotel) + parseFloat(kaltitotel)
+//     console.log("add")
+
+//     let input = document.getElementById("totelbill").value = add
+//     alert("thanks for use")
+
+// }
+
+// function Click() {
+//     let rotaryint = parseFloat(document.getElementById("rotary").value);
+//     document.getElementById("rotary").value
+//     let thresher = parseFloat(document.getElementById("thresher").value);
+//     let kaltiint = parseFloat(document.getElementById("kalti").value);
+
+//     let rotarytotel = rotaryint * .1300;
+//     let threshertotel = thresher * 1200;
+//     let kaltitotel = kaltiint * 900;
+
+//     let add = rotarytotel + threshertotel + kaltitotel;
+//     console.log(add);
+
+//     document.getElementById("totelbill").value = add;
+//     alert("Thanks for using!");
+// }
